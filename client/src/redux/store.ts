d5fd7cb991ab50c6b12ff52e848a618635c9d783
shaikh faiz <faizuddinshaikh55@gmail.com>
2025-05-31 21:31:19 +0530

    errors solved

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,43 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { authApi } from "./authApi";
-import authSlice from "./slices/authSlice";
-import { transactionApi } from "./transactionApi";
-import { planApi } from "./planApi";
-import { subscriptionApi } from "./subscriptionApi";
-import { userApi } from "./userApi";
-import { categoryApi } from "./categoryApi";
+// import { configureStore } from "@reduxjs/toolkit";
+// import { authApi } from "./authApi";
+// import authSlice from "./slices/authSlice";
+// import { transactionApi } from "./transactionApi";
+// import { planApi } from "./planApi";
+// import { subscriptionApi } from "./subscriptionApi";
+// import { userApi } from "./userApi";
+// import { categoryApi } from "./categoryApi";
 
+// const reduxStore = configureStore({
+//   reducer: {
+//     [authApi.reducerPath]: authApi.reducer,
+//     [transactionApi.reducerPath]: transactionApi.reducer,
+//     [planApi.reducerPath]: planApi.reducer,
+//     [subscriptionApi.reducerPath]: subscriptionApi.reducer,
+//     [userApi.reducerPath]: userApi.reducer,
+//     [categoryApi.reducerPath]: categoryApi.reducer,
+// auth :    authSlice
 
+//   },
+//   middleware: (getDefaultMiddleware:any) =>
+//     getDefaultMiddleware().concat(
+//       authApi.middleware,
+//       transactionApi.middleware,
+//       planApi.middleware,
+//       subscriptionApi.middleware,
+//       userApi.middleware,
+//       categoryApi.middleware,
 
+//     )
+// })
 
+// export type RootState = ReturnType<typeof reduxStore.getState>
+// export type AppDispatch = typeof reduxStore.dispatch
 
-const reduxStore = configureStore({
-  reducer: {
-    [authApi.reducerPath]: authApi.reducer,
-    [transactionApi.reducerPath]: transactionApi.reducer,
-    [planApi.reducerPath]: planApi.reducer,
-    [subscriptionApi.reducerPath]: subscriptionApi.reducer,
-    [userApi.reducerPath]: userApi.reducer,
-    [categoryApi.reducerPath]: categoryApi.reducer,
-auth :    authSlice
-
-  
-  },
-  middleware: (getDefaultMiddleware:any) =>
-    getDefaultMiddleware().concat(
-      authApi.middleware,
-      transactionApi.middleware,
-      planApi.middleware,
-      subscriptionApi.middleware,
-      userApi.middleware,
-      categoryApi.middleware,
-  
-    )
-})
-
-
-export type RootState = ReturnType<typeof reduxStore.getState>
-export type AppDispatch = typeof reduxStore.dispatch
-
-export default reduxStore
-
+// export default reduxStore
--- client/src/redux/userApi.ts
+++ client/src/redux/userApi.ts
@@ -1,74 +1,74 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { RootState } from "./store";
-// import { IUser } from "../models/user.interface";
-interface IContact {
-  name: string;
-  email: string;
-  message: string;
-  mobile: string;
-}
-// Custom base query to fetch token from Redux store
-const baseQuery = fetchBaseQuery({
-  // baseUrl: "http://localhost:5000/api/v1/user",
-  baseUrl: `${import.meta.env.VITE_BACKEND_URL}/api/v1/contact`,
-  credentials: "include",
-  prepareHeaders: (headers, { getState }) => {
-    const state = getState() as RootState;
-    const token = state.auth.token;
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { RootState } from "./store";
+// // import { IUser } from "../models/user.interface";
+// interface IContact {
+//   name: string;
+//   email: string;
+//   message: string;
+//   mobile: string;
+// }
+// // Custom base query to fetch token from Redux store
+// const baseQuery = fetchBaseQuery({
+//   // baseUrl: "http://localhost:5000/api/v1/user",
+//   baseUrl: `${import.meta.env.VITE_BACKEND_URL}/api/v1/contact`,
+//   credentials: "include",
+//   prepareHeaders: (headers, { getState }) => {
+//     const state = getState() as RootState;
+//     const token = state.auth.token;
 
-    if (token) {
-      headers.set("Authorization", `Bearer ${token}`);
-    }
-    return headers;
-  },
-});
+//     if (token) {
+//       headers.set("Authorization", `Bearer ${token}`);
+//     }
+//     return headers;
+//   },
+// });
 
-// Create API
-export const userApi = createApi({
-  reducerPath: "userApi",
-  baseQuery,
-  endpoints: (builder) => ({
-    // Fetch all users with pagination
-    getAllUsers: builder.query<{ message: string; result: IContact }, void>({
-      query: () => ({
-        url: "/all-contacts",
-        method: "GET",
-      }),
-      transformResponse(data: {
-        message: string;
-        result: {
-          name: string;
-          email: string;
-          message: string;
-          mobile: string;
-        };
-      }) {
-        return data;
-      },
-      transformErrorResponse: (error: {
-        status: number;
-        data: { message: string };
-      }) => error.data.message,
-    }),
-    contact: builder.mutation<{ message: string }, IContact>({
-      query: (data) => ({
-        url: "/contact",
-        method: "POST",
-        body: data,
-      }),
-      transformResponse(data: {
-        message: string;
-        
-      }) {
-        return data;
-      },
-      transformErrorResponse: (error: {
-        status: number;
-        data: { message: string };
-      }) => error.data.message,
-    }),
-  }),
-});
+// // Create API
+// export const userApi = createApi({
+//   reducerPath: "userApi",
+//   baseQuery,
+//   endpoints: (builder) => ({
+//     // Fetch all users with pagination
+//     getAllUsers: builder.query<{ message: string; result: IContact }, void>({
+//       query: () => ({
+//         url: "/all-contacts",
+//         method: "GET",
+//       }),
+//       transformResponse(data: {
+//         message: string;
+//         result: {
+//           name: string;
+//           email: string;
+//           message: string;
+//           mobile: string;
+//         };
+//       }) {
+//         return data;
+//       },
+//       transformErrorResponse: (error: {
+//         status: number;
+//         data: { message: string };
+//       }) => error.data.message,
+//     }),
+//     contact: builder.mutation<{ message: string }, IContact>({
+//       query: (data) => ({
+//         url: "/contact",
+//         method: "POST",
+//         body: data,
+//       }),
+//       transformResponse(data: {
+//         message: string;
 
-// Export hooks
-export const { useGetAllUsersQuery } = userApi;
+//       }) {
+//         return data;
+//       },
+//       transformErrorResponse: (error: {
+//         status: number;
+//         data: { message: string };
+//       }) => error.data.message,
+//     }),
+//   }),
+// });
+
+// // Export hooks
+// export const { useGetAllUsersQuery } = userApi;
